4aa30abb371d8cfa10a5ee0386490c33abebf050
rominagatica <rom.gatica@gmail.com>
2019-02-26 08:57:51 -0300

    acepto cambio de funciones

--- src/data.js
+++ src/data.js
@@ -1,10 +1,33 @@
 /* Manejo de data */
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+//crear una funcion que filtre tipo de pokemones//
 
-const example = () => {
+const example = ()  => {
   return 'example';
 };
 
 window.example = example;
+
+//crear una función que filtre por debilidades de pokemones//
+
+
+//crear una funcion que ordene por nombre de pokemon, de la A-Z y de la Z-A//
+const dataorder = (data, condition) => {
+  let order = [];
+  if (condition === 'az') {
+    order = data.sort(orderByName);
+  }
+  if (condition === 'za') {
+    order = data.sort(orderByName).reverse();
+}
+if (condition === 'idasc') {
+  order = data.sort(orderById);
+}
+if (condition === 'iddesc') {
+  order = data.sort(orderById).reverse();
+}
+
+return ordered;
+
+};
+//document.get
