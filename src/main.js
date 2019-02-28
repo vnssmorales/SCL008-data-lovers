a9ad8f9c12f0592f1832c280519bcacf4629df6b
rominagatica <rom.gatica@gmail.com>
2019-02-28 12:53:30 -0300

    agrego boton por nombre

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 /* Manejo del DOM */
 
-/*const btnStart = document.getElementsByTagName('button')[0];
-
-btnStart.addEventListener('click', () => {
-document.getElementById('root').innerHTML = '';
-document.getElementById('root').innerHTML += `<h3 class="title">Este es Bulbasaur</h3>
-<img src="http://www.serebii.net/pokemongo/pokemon/001.png" alt="Bulbasaur">`
-for (let i = 0; i < names1.length; i++) {
-document.getElementById('root').innerHTML += `<p>${names1[i]}</p>`
-}
-}); */
 
 const data = window.POKEMON.pokemon;
 const container = document.getElementById('root');
@@ -17,9 +7,13 @@ container.innerHTML ="";
 
 const showData = (data) => {
     data.forEach(element => {
-        container.innerHTML += `<p>${element.name}</p>
-        <img class="image" src="${element.img}" alt="${element.weaknesses}">
-        <p>${element.weaknesses}</p>`
+        container.innerHTML += `<div class="card">
+        <img src="${element.img}" class="card-img-top" alt="${element.weaknesses}">
+        <div class="card-body">
+        <p class="card-text">${element.name}</p>
+        <p class="card-text">${element.weaknesses}</p>
+        </div>
+        </div>`
 })
 }
 
@@ -29,9 +23,13 @@ document.getElementById('select-weaknesses').addEventListener('change', () => {
 
  container.innerHTML ="";
  result.forEach(element => {
-     container.innerHTML += `<p>${element.name}</p> 
-        <img class="image" src="${element.img}" alt="${element.weaknesses}">
-        <p>${element.weaknesses}</p>`
+     container.innerHTML += `<div class="card">
+     <img src="${element.img} " class="card-img-top" alt"${element.weaknesses}">
+     <div class="card-body">
+     <p class="card-text">${element.name}</p>   
+     <p class="card-text">${element.weaknesses}
+     </div>
+     </div>`
  })
 })
 ;
